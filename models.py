5f7a999db0c3b659ef58cf61f51b2f15964269a9
Josiel Phelipe <josiephelipel265@gmail.com>
2025-11-10 17:06:06 -0400

    atualiza models.py

--- models.py
+++ models.py
@@ -1,18 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'
+    __tablename__ = 'Usuario'
     
-    id = db.Column(db.Integer, primary_key=True)
+    ID = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    usuario = db.Column(db.String(50), unique=True, nullable=False)
-    cpf = db.Column(db.String(14), unique=True, nullable=False)
-    telefone = db.Column(db.String(15), nullable=False)
+    CPF = db.Column(db.String(14), unique=True, nullable=False)
+    nome_usuario = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    tipo_usuario = db.Column(db.String(20), nullable=False, default='participante')
+    telefone = db.Column(db.String(20), nullable=False)
     senha = db.Column(db.String(255), nullable=False)
+    data_criacao = db.Column(db.TIMESTAMP, server_default=db.func.now())
     
     def set_senha(self, senha):
         self.senha = generate_password_hash(senha)
@@ -22,10 +25,28 @@ class Usuario(db.Model):
     
     def to_dict(self):
         return {
-            'id': self.id,
+            'id': self.ID,
             'nome': self.nome,
-            'usuario': self.usuario,
-            'cpf': self.cpf,
+            'nome_usuario': self.nome_usuario,
+            'email': self.email,
             'telefone': self.telefone,
-            'email': self.email
+            'cpf': self.CPF,
+            'data_criacao': self.data_criacao
         }
+
+class Organizador(db.Model):
+    __tablename__ = 'Organizador'
+    
+    ID = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    cpf_cnpj = db.Column(db.String(20), unique=True, nullable=False)
+    telefone = db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
+    data_criacao = db.Column(db.TIMESTAMP, server_default=db.func.now())
+    
+    def set_senha(self, senha):
+        self.senha = generate_password_hash(senha)
+    
+    def check_senha(self, senha):
+        return check_password_hash(self.senha, senha)
