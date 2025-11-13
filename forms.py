4eb2b4b6dec796e78fe6f1ee8551d447463adbe1
Josiel Phelipe <josiephelipel265@gmail.com>
2025-11-13 13:51:52 -0400

    cria formulário de atividades

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField, DateField, TimeField
-from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, Optional
 from datetime import datetime
-from models import Usuario, Organizador
+from models import Usuario, Organizador, Atividades
 import re
 
 class CadastroForm(FlaskForm):
@@ -63,3 +63,21 @@ class EventoForm(FlaskForm):
     def validate_data(self, field):
         if field.data < datetime.now().date():
             raise ValidationError('A data do evento não pode ser no passado.') 
+
+class AtividadeForm(FlaskForm):
+    titulo = StringField('Título da Atividade', validators=[DataRequired(), Length(max=100)])
+    descricao = TextAreaField('Descrição', validators=[DataRequired()])
+    data = DateField('Data da Atividade', validators=[DataRequired()], format='%Y-%m-%d')
+    horario_inicio = TimeField('Horário de Início', validators=[DataRequired()], format='%H:%M')
+    horario_fim = TimeField('Horário de Término', validators=[DataRequired()], format='%H:%M')
+    convidado = StringField('Convidado/Palestrante', validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField('Adicionar Atividade')
+    
+    def validate_data(self, field):
+        if field.data < datetime.now().date():
+            raise ValidationError('A data da atividade não pode ser no passado.')
+    
+    def validate_horario_fim(self, field):
+        if self.horario_inicio.data and field.data:
+            if field.data <= self.horario_inicio.data:
+                raise ValidationError('O horário de término deve ser após o horário de início.')
